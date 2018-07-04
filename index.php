591ecf0948d1fddcac4cb238f82b670cdaa412ca
AzenoX <azen0x.alexis@gmail.com>
2018-07-04 23:11:37 +0200

    Upload1
    ouais

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>BTP-roject</title>
+    <meta charset="utf-8">
+    <meta name="language" content="fr-FR" />
+
+    
+</head>    
+
+    
+<?php
+
+echo 'SALU À TOUSSE';
+
+?>
+
+
+</html>
